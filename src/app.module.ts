a702a9ee02badcdd0aea7d63ab2232b58f4d2d35
Lmh98 <1004alsgud@gmail.com>
2024-09-13 21:31:20 +0900

    feat: db 환경변수 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,30 @@
 import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { RestaurantsController } from './restaurants/restaurants.controller';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
-  imports: [UsersModule, RestaurantsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.development.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity.{ts,js}'],
+      synchronize: false,
+    }),
+    UsersModule,
+    RestaurantsModule,
+  ],
   controllers: [UsersController, RestaurantsController],
   providers: [],
 })
