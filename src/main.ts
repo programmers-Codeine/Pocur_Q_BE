cbe0e156bac604287c487ba1cee1c5f963b22290
Lmh98 <1004alsgud@gmail.com>
2024-09-20 13:34:37 +0900

    feat: main.ts 유효성 검사 파이프 추가

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Enable CORS if needed
-  app.useLogger(console); // Detailed logging
+
+  app.enableCors();
+  app.useLogger(console);
+
+  // 유효성 검사 파이프 추가
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 정의되지 않은 속성 제거
+      forbidNonWhitelisted: true, // DTO에 정의되지 않은 속성일 경우 에러 발생
+      transform: true, // DTO로 변환
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
