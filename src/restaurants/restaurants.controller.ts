e816a495433c58883cb1c58446ef8f21feca646e
Lmh98 <1004alsgud@gmail.com>
2024-09-20 13:57:50 +0900

    feat: restaurants API

--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param } from '@nestjs/common';
 import { RestaurantsService } from './restaurants.service';
+import { CreateRestaurantDto } from './dto/create-restaurants.dto';
+import { UpdateRestaurantDto } from './dto/update-restaurants.dto';
+import { Restaurant } from './entities/restaurants.entity';
 
 @Controller('restaurants')
 export class RestaurantsController {
   constructor(private readonly restaurantsService: RestaurantsService) {}
 
-  @Get()
-  getRestaurants(): string {
-    return this.restaurantsService.getRestaurants();
+  @Get(':restaurant_id')
+  async getRestaurant(@Param('restaurant_id') restaurantId: string): Promise<Restaurant> {
+    return await this.restaurantsService.getRestaurantById(restaurantId);
+  }
+
+  @Post(':user_id')
+  async createRestaurant(
+    @Param('user_id') userId: string,
+    @Body() createRestaurantDto: CreateRestaurantDto,
+  ): Promise<Restaurant> {
+    return await this.restaurantsService.createRestaurant(userId, createRestaurantDto);
+  }
+
+  @Put(':restaurant_id')
+  async updateRestaurant(
+    @Param('restaurant_id') restaurantId: string,
+    @Body() updateRestaurantDto: UpdateRestaurantDto,
+  ): Promise<Restaurant> {
+    return await this.restaurantsService.updateRestaurant(restaurantId, updateRestaurantDto);
   }
 }
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,8 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Restaurant } from './entities/restaurants.entity';
+import { CreateRestaurantDto } from './dto/create-restaurants.dto';
+import { UpdateRestaurantDto } from './dto/update-restaurants.dto';
 
 @Injectable()
 export class RestaurantsService {
-  getRestaurants(): string {
-    return 'RestaurantsService 111';
+  constructor(
+    @InjectRepository(Restaurant)
+    private readonly restaurantRepository: Repository<Restaurant>,
+  ) {}
+
+  async getRestaurantById(restaurantId: string): Promise<Restaurant> {
+    const restaurant = await this.restaurantRepository.findOne({ where: { id: restaurantId } });
+    if (!restaurant) {
+      throw new NotFoundException(`Restaurant with ID ${restaurantId} not found`);
+    }
+    return restaurant;
+  }
+
+  async createRestaurant(userId: string, createRestaurantDto: CreateRestaurantDto): Promise<Restaurant> {
+    const newRestaurant = this.restaurantRepository.create({
+      ...createRestaurantDto,
+      user_id: userId,
+    });
+    return await this.restaurantRepository.save(newRestaurant);
+  }
+
+  async updateRestaurant(restaurantId: string, updateRestaurantDto: UpdateRestaurantDto): Promise<Restaurant> {
+    const restaurant = await this.restaurantRepository.findOne({ where: { id: restaurantId } });
+
+    if (!restaurant) {
+      throw new NotFoundException(`Restaurant with ID ${restaurantId} not found`);
+    }
+
+    Object.assign(restaurant, updateRestaurantDto);
+    return await this.restaurantRepository.save(restaurant);
   }
 }
