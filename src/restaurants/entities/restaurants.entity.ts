d91d3d8ebe6b068ef0809bb7b9c260d438ed12a2
Lmh98 <1004alsgud@gmail.com>
2024-09-20 13:56:50 +0900

    feat: restaurants entity

---
+++ src/restaurants/entities/restaurants.entity.ts
@@ -0,0 +1,41 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { IsString, IsInt, MaxLength } from 'class-validator';
+
+@Entity('restaurants')
+export class Restaurant {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 45 })
+  @IsString()
+  @MaxLength(45)
+  user_id: string;
+
+  @Column({ type: 'varchar', length: 45 })
+  @IsString()
+  @MaxLength(45)
+  name: string;
+
+  @Column({ type: 'int', default: null })
+  @IsInt()
+  default_table_count: number;
+
+  @Column({ type: 'int', default: null })
+  @IsInt()
+  total_table_count: number;
+
+  @Column({ type: 'varchar', length: 45, nullable: true })
+  @IsString()
+  @MaxLength(45)
+  logo: string;
+
+  @Column({ type: 'varchar', length: 45, nullable: true })
+  @IsString()
+  @MaxLength(45)
+  introduce: string;
+
+  @Column({ type: 'varchar', length: 45, nullable: true })
+  @IsString()
+  @MaxLength(45)
+  comment: string;
+}
