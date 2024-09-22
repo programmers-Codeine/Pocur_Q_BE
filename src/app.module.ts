a4792f5a66c7369f653f03a3c9a46c542d7a6cc5
Lmh98 <1004alsgud@gmail.com>
2024-09-22 14:31:17 +0900

    chore: typeorm logging옵션 true로 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,6 +24,7 @@ dotenv.config();
       database: process.env.DB_DATABASE,
       entities: [__dirname + '/**/*.entity.{ts,js}'],
       synchronize: false,
+      logging: true,
     }),
     UsersModule,
     RestaurantsModule,
