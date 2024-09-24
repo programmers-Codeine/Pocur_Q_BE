bd2e0b8b0e62a44549d08da628f13f41ba6258dc
hyoeun0001 <83484611+hyoeun0001@users.noreply.github.com>
2024-09-24 16:55:37 +0900

    refactor: 첫번째 로그인 확인 로직 추가 isFirstLogin

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,16 +5,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './entities/users.entity';
 import { JwtModule } from '@nestjs/jwt';
 import * as dotenv from 'dotenv';
+import { Restaurant } from 'src/restaurants/entities/restaurants.entity';
+import { RestaurantsModule } from 'src/restaurants/restaurants.module';
 
 dotenv.config();
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Users]),
+    TypeOrmModule.forFeature([Users, Restaurant]),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1h' },
     }),
+    RestaurantsModule,
   ],
   controllers: [UsersController],
   providers: [UsersService],
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,12 +6,15 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { LoginUserDto } from './dtos/login-user.dto';
 import { JwtService } from '@nestjs/jwt';
+import { Restaurant } from 'src/restaurants/entities/restaurants.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(Users)
     private readonly userRepository: Repository<Users>,
+    @InjectRepository(Restaurant)
+    private readonly restaurantRepository: Repository<Restaurant>,
     private readonly jwtService: JwtService,
   ) {}
 
@@ -35,7 +38,7 @@ export class UsersService {
     }
   }
 
-  async login(loginDto: LoginUserDto): Promise<{ accessToken: string }> {
+  async login(loginDto: LoginUserDto): Promise<{ accessToken: string; isFirstLogin: boolean }> {
     const { email, password } = loginDto;
     const user = await this.userRepository.findOne({ where: { email } });
 
@@ -48,9 +51,15 @@ export class UsersService {
       throw new UnauthorizedException('인증에 실패했습니다.');
     }
 
+    // restaurants 테이블에서 해당 유저가 있는지 확인
+    const isUserInRestaurants = await this.restaurantRepository.findOne({ where: { user_id: user.id } });
+
+    // user가 없으면 처음 로그인한 것으로 간주
+    const isFirstLogin = !isUserInRestaurants;
+
     const payload = { email: user.email, sub: user.id, nickname: user.nickname };
     const accessToken = this.jwtService.sign(payload);
 
-    return { accessToken };
+    return { accessToken, isFirstLogin };
   }
 }
