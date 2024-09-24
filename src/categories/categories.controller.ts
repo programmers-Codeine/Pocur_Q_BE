3c86f866ac19391bf41d699a82f9b4cf43a602a9
hyoeun0001 <83484611+hyoeun0001@users.noreply.github.com>
2024-09-24 11:48:18 +0900

    feat: 카테고리 추가 api 구현

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { Categories } from './entities/categories.entity';
+import { CreateCategoryDto } from './dtos/create-categories.dto';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
   @Get(':restaurant_id')
-  async getCategory(@Param('restaurant_id') restaurantId: string): Promise<Categories> {
+  async getCategory(@Param('restaurant_id') restaurantId: string): Promise<Categories[]> {
     return await this.categoriesService.getCategory(restaurantId);
+  }
+
+  @Post(':restaurant_id')
+  async createCategory(
+    @Param('restaurant_id') restaurantId: string,
+    @Body() createCategoryDto: CreateCategoryDto,
+  ): Promise<Categories> {
+    return await this.categoriesService.createCategory(restaurantId, createCategoryDto);
   }
 }
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Categories } from './entities/categories.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateCategoryDto } from './dtos/create-categories.dto';
 
 @Injectable()
 export class CategoriesService {
@@ -10,12 +11,18 @@ export class CategoriesService {
     private readonly categoryRepository: Repository<Categories>,
   ) {}
 
-  async getCategory(restaurantId: string): Promise<Categories> {
-    const categories = await this.categoryRepository.findOne({ where: { restaurant_id: restaurantId } });
+  async getCategory(restaurantId: string): Promise<Categories[]> {
+    const categories = await this.categoryRepository.find({ where: { restaurant_id: restaurantId } });
     if (!categories) {
       throw new NotFoundException('레스토랑의 카테고리를 찾지 못했습니다.');
     }
 
     return categories;
+  }
+
+  async createCategory(restaurantId: string, createCategoryDto: CreateCategoryDto): Promise<Categories> {
+    const newCategory = await this.categoryRepository.create({ ...createCategoryDto, restaurant_id: restaurantId });
+
+    return await this.categoryRepository.save(newCategory);
   }
 }
