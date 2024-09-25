fb3bf4863163e1a66493c78cac80c0192c6aa2b6
hyoeun0001 <83484611+hyoeun0001@users.noreply.github.com>
2024-09-25 21:34:50 +0900

    refactor: UseGuards 추가

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,17 +1,20 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { Categories } from './entities/categories.entity';
 import { CreateCategoryDto } from './dtos/create-categories.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get(':restaurant_id')
   async getCategory(@Param('restaurant_id') restaurantId: string): Promise<Categories[]> {
     return await this.categoriesService.getCategory(restaurantId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post(':restaurant_id')
   async createCategory(
     @Param('restaurant_id') restaurantId: string,
@@ -20,6 +23,7 @@ export class CategoriesController {
     return await this.categoriesService.createCategory(restaurantId, createCategoryDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':restaurant_id/:category_id')
   async updateCategory(
     @Param('restaurant_id') restaurantId: string,
@@ -29,6 +33,7 @@ export class CategoriesController {
     return await this.categoriesService.updateCategory(restaurantId, categoryId, createCategoryDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':restaurant_id/:category_id')
   async deleteCategory(
     @Param('restaurant_id') restaurantId: string,
