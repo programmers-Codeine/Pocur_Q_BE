73b7750f83bedcd889b5cace02015dbeb87de235
Lmh98 <1004alsgud@gmail.com>
2024-09-26 01:38:38 +0900

    feat: 레스토랑의 모든 주문내역 조회 api

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Post, Body, Param } from '@nestjs/common';
+import { Controller, Post, Body, Param, Get } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-orders.dto';
 import { Order } from './entities/orders.entity';
+import { OrderSummaryDto } from './dto/order-summary.dto';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
+
+  @Get(':restaurant_id')
+  async findAllOrdersByRestaurant(@Param('restaurant_id') restaurant_id: string): Promise<OrderSummaryDto[]> {
+    return this.ordersService.findAllOrdersByRestaurant(restaurant_id);
+  }
 
   @Post(':restaurant_id/:restaurantTable_id')
   async createOrder(
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -6,6 +6,7 @@ import { CreateOrderDto } from './dto/create-orders.dto';
 import { Menus } from 'src/menus/entities/menus.entity';
 import { Restaurant } from 'src/restaurants/entities/restaurants.entity';
 import { RestaurantTable } from 'src/restaurantTables/entities/restaurantTables.entity';
+import { OrderSummaryDto } from './dto/order-summary.dto';
 
 @Injectable()
 export class OrdersService {
@@ -22,6 +23,26 @@ export class OrdersService {
     @InjectRepository(RestaurantTable)
     private restaurantTableRepository: Repository<RestaurantTable>,
   ) {}
+
+  async findAllOrdersByRestaurant(restaurant_id: string): Promise<OrderSummaryDto[]> {
+    const orders = await this.ordersRepository.find({
+      where: { restaurant: { id: restaurant_id } },
+      relations: ['menu', 'restaurantTable'],
+    });
+
+    if (!orders.length) {
+      throw new NotFoundException(`No orders found for restaurant with id ${restaurant_id}`);
+    }
+
+    // 필요한 정보만 추출하여 반환
+    return orders.map((order) => ({
+      id: order.id,
+      table_num: order.restaurantTable.table_num,
+      menu_name: order.menu.menu_name,
+      price: order.menu.price,
+      count: order.count,
+    }));
+  }
 
   async createOrder(createOrderDto: CreateOrderDto, restaurant_id: string, restaurantTable_id: string): Promise<Order> {
     const { menu_id, count } = createOrderDto;
