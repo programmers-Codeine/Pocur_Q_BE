1007812bcb2ccac681025d4fb88a60587249f9db
Lmh98 <1004alsgud@gmail.com>
2024-10-01 16:18:20 +0900

    refactor: response 카멜케이스로 변경

--- src/restaurantTables/entities/restaurantTables.entity.ts
+++ src/restaurantTables/entities/restaurantTables.entity.ts
@@ -1,19 +1,45 @@
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn, OneToMany } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+  OneToMany,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { Restaurant } from 'src/restaurants/entities/restaurants.entity';
 import { Order } from 'src/orders/entities/orders.entity';
+import { Expose } from 'class-transformer';
 
 @Entity('restaurantTables')
 export class RestaurantTable {
   @PrimaryGeneratedColumn('uuid')
+  @Expose()
   id: string;
 
   @ManyToOne(() => Restaurant, (restaurant) => restaurant.tables, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'restaurant_id' })
+  @Expose({ name: 'restaurantId' })
   restaurant: Restaurant;
 
   @Column()
+  @Expose({ name: 'tableNum' })
   table_num: number;
 
   @OneToMany(() => Order, (order) => order.restaurantTable)
+  @Expose()
   orders: Order[];
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', name: 'created_at' })
+  @Expose({ name: 'createdAt' })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
+    name: 'updated_at',
+  })
+  @Expose({ name: 'updatedAt' })
+  updated_at: Date;
 }
