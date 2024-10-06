ddd49121f081597cb2efb178e215a3fe421e06ee
Lmh98 <1004alsgud@gmail.com>
2024-10-06 15:59:20 +0900

    refactor: response 카멜 케이스로 변경 및 엔티티 수정

--- src/orders/entities/orders.entity.ts
+++ src/orders/entities/orders.entity.ts
@@ -7,39 +7,47 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { Expose } from 'class-transformer';
 import { Restaurant } from 'src/restaurants/entities/restaurants.entity';
-import { RestaurantTable } from 'src/restaurantTables/entities/restaurantTables.entity';
 import { Menu } from 'src/menus/entities/menus.entity';
 
 @Entity('orders')
 export class Order {
   @PrimaryGeneratedColumn('uuid')
+  @Expose()
   id: string;
 
+  @Column({ type: 'int', nullable: true })
+  @Expose({ name: 'tableNum' })
+  table_num: number;
+
   @Column({ type: 'timestamp', nullable: true })
+  @Expose({ name: 'orderedAt' })
   ordered_at: Date;
 
   @Column({ type: 'int', nullable: true })
+  @Expose()
   count: number;
 
   @Column({ type: 'int', nullable: true })
+  @Expose({ name: 'totalPrice' })
   total_price: number;
 
   @CreateDateColumn({ type: 'timestamp' })
+  @Expose({ name: 'createdAt' })
   created_at: Date;
 
   @UpdateDateColumn({ type: 'timestamp' })
+  @Expose({ name: 'updatedAt' })
   updated_at: Date;
 
   @ManyToOne(() => Restaurant, (restaurant) => restaurant.orders)
   @JoinColumn({ name: 'restaurant_id' })
+  @Expose()
   restaurant: Restaurant;
-
-  @ManyToOne(() => RestaurantTable, (restaurantTable) => restaurantTable.orders)
-  @JoinColumn({ name: 'restaurantTable_id' })
-  restaurantTable: RestaurantTable;
 
   @ManyToOne(() => Menu, (menu) => menu.orders)
   @JoinColumn({ name: 'menu_id' })
+  @Expose()
   menu: Menu;
 }
