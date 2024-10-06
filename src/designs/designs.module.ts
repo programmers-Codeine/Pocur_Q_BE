136215ed830da81850e9b868987ca5e1ab6579f8
Lmh98 <1004alsgud@gmail.com>
2024-10-06 23:02:48 +0900

    feat: designs 엔티티 생성

--- src/designs/designs.module.ts
+++ src/designs/designs.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DesignsController } from './designs.controller';
 import { DesignsService } from './designs.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Design } from './entities/designs.entity';
+import { Restaurant } from 'src/restaurants/entities/restaurants.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Design, Restaurant])],
   controllers: [DesignsController],
   providers: [DesignsService],
   exports: [DesignsService],
