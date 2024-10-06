c497c615127f07077c3b35a167409d814e31d3db
Lmh98 <1004alsgud@gmail.com>
2024-10-06 23:39:50 +0900

    feat: 디자인 조회 메소드

--- src/designs/designs.controller.ts
+++ src/designs/designs.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { DesignsService } from './designs.service';
+import { Design } from './entities/designs.entity';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('designs')
-export class DesignsController {}
+export class DesignsController {
+  constructor(private readonly designsService: DesignsService) {}
+
+  @Get()
+  async getDesign(@Request() req): Promise<Design> {
+    const restaurantId = req.user.restaurantId;
+
+    return this.designsService.getDesign(restaurantId);
+  }
+}
--- src/designs/designs.service.ts
+++ src/designs/designs.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Design } from './entities/designs.entity';
 
 @Injectable()
-export class DesignsService {}
+export class DesignsService {
+  constructor(
+    @InjectRepository(Design)
+    private readonly designsRepository: Repository<Design>,
+  ) {}
+
+  async getDesign(restaurantId: string): Promise<Design> {
+    const design = await this.designsRepository.findOne({
+      where: { restaurant: { id: restaurantId } },
+      relations: ['restaurant'],
+    });
+
+    if (!design) {
+      throw new NotFoundException(`레스토랑 ID가 ${restaurantId}인 디자인을 찾을 수 없습니다.`);
+    }
+
+    return design;
+  }
+}
