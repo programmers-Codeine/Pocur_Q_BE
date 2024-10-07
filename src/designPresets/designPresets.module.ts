9b7b6a8e9d4662df772833e3ce08093fe0513ca4
Lmh98 <1004alsgud@gmail.com>
2024-10-07 22:03:08 +0900

    feat: designPresets 전체조회, 개별조회, 생성, 수정, 삭제 메소드

--- src/designPresets/designPresets.module.ts
+++ src/designPresets/designPresets.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DesignPresetsController } from './designPresets.controller';
 import { DesignPresetsService } from './designPresets.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DesignPreset } from './entities/designPresets.entity';
+import { Restaurant } from 'src/restaurants/entities/restaurants.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DesignPreset, Restaurant])],
   controllers: [DesignPresetsController],
   providers: [DesignPresetsService],
+  exports: [DesignPresetsService],
 })
 export class DesignPresetsModule {}
--- src/designPresets/designPresets.service.ts
+++ src/designPresets/designPresets.service.ts
@@ -1,4 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { DesignPreset } from './entities/designPresets.entity';
+import { CreateDesignPresetDto } from './dto/create-designPresets.dto';
+import { UpdateDesignPresetDto } from './dto/update-designPresets.dto';
+import { Restaurant } from 'src/restaurants/entities/restaurants.entity';
 
 @Injectable()
-export class DesignPresetsService {}
+export class DesignPresetsService {
+  constructor(
+    @InjectRepository(DesignPreset)
+    private readonly designPresetsRepository: Repository<DesignPreset>,
+
+    @InjectRepository(Restaurant)
+    private readonly restaurantRepository: Repository<Restaurant>,
+  ) {}
+
+  async getDesignPresets(restaurantId: string): Promise<DesignPreset[]> {
+    const restaurant = await this.restaurantRepository.findOne({ where: { id: restaurantId } });
+
+    if (!restaurant) {
+      throw new NotFoundException(`ID가 ${restaurantId}인 레스토랑을 찾을 수 없습니다.`);
+    }
+
+    return this.designPresetsRepository.find({ where: { restaurant } });
+  }
+
+  async getDesignPresetById(designPresetId: string): Promise<DesignPreset> {
+    const designPreset = await this.designPresetsRepository.findOne({ where: { id: designPresetId } });
+
+    if (!designPreset) {
+      throw new NotFoundException(`ID가 ${designPresetId}인 디자인 프리셋을 찾을 수 없습니다.`);
+    }
+
+    return designPreset;
+  }
+
+  async createDesignPreset(restaurantId: string, createDesignPresetDto: CreateDesignPresetDto): Promise<DesignPreset> {
+    const restaurant = await this.restaurantRepository.findOne({ where: { id: restaurantId } });
+
+    if (!restaurant) {
+      throw new NotFoundException(`ID가 ${restaurantId}인 레스토랑을 찾을 수 없습니다.`);
+    }
+
+    const newDesignPreset = this.designPresetsRepository.create({
+      ...createDesignPresetDto,
+      restaurant,
+    });
+
+    return this.designPresetsRepository.save(newDesignPreset);
+  }
+
+  async updateDesignPreset(
+    designPresetId: string,
+    updateDesignPresetDto: UpdateDesignPresetDto,
+  ): Promise<DesignPreset> {
+    const designPreset = await this.designPresetsRepository.findOne({ where: { id: designPresetId } });
+
+    if (!designPreset) {
+      throw new NotFoundException(`ID가 ${designPresetId}인 디자인 프리셋을 찾을 수 없습니다.`);
+    }
+    const updatedDesignPreset = this.designPresetsRepository.merge(designPreset, updateDesignPresetDto);
+
+    return this.designPresetsRepository.save(updatedDesignPreset);
+  }
+
+  async deleteDesignPreset(designPresetId: string): Promise<void> {
+    const result = await this.designPresetsRepository.delete(designPresetId);
+
+    if (result.affected === 0) {
+      throw new NotFoundException(`ID가 ${designPresetId}인 디자인 프리셋을 찾을 수 없습니다.`);
+    }
+  }
+}
